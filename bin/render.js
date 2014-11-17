b58eee81f1e7a6f39c6b5f3a748c17b11835bb75
oncletom <hi@oncletom.io>
2014-11-17 16:17:28 +0100

    Browserify workflow.
    We don't want LESS compiling for the static rendering. So we disable any `require('*.less')` call.

--- bin/render.js
+++ bin/render.js
@@ -2,23 +2,31 @@
 
 require("node-jsx").install({ extension: ".jsx" });
 
+// neutralizes any require('foo/bar.less');
+require.extensions['.less'] = function(){
+  return '';
+};
+
 var fs = require('fs');
 var path = require('path');
-
+var through = require('through2');
 var React = require('react');
 
 var App = require('../app.js');
 var repos = require('../data/repos.json');
 
 var body = React.renderToString(React.createElement(App, { repos: repos }));
+var inputFile = path.join(__dirname, '..', 'src', 'index.html');
+var outputFile = path.join(__dirname, '..', 'dist', 'index.html');
 
-fs.readFile(path.join(__dirname, '..', 'src', 'index.html'), function(err, binaryContent){
-  if (err){
-    throw err;
-  }
+fs.createReadStream(inputFile)
+  .pipe(through(function(chunk, enc, done){
+    chunk = String(chunk)
 
-  fs.writeFile(
-    path.join(__dirname, '..', 'dist', 'index.html'),
-    String(binaryContent).replace('<main></main>', '<main>' + body + '</main>')
-  );
-});
+    if (chunk.indexOf('<main>') !== -1){
+      chunk = chunk.replace(/(<main>)/, '$1' + body);
+    }
+
+    done(null, chunk);
+  }))
+  .pipe(fs.createWriteStream(outputFile));
