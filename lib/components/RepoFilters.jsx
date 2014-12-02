5a976afd99b9b455e160aef5b3436ce6340189de
oncletom <hi@oncletom.io>
2014-12-02 16:36:33 +0300

    Search filter.

--- lib/components/RepoFilters.jsx
+++ lib/components/RepoFilters.jsx
@@ -2,6 +2,7 @@
 
 var React = require('react');
 var Language = require('./Language.jsx');
+var cx = require('react-classset');
 
 module.exports  = React.createClass({
   propTypes: {
@@ -22,19 +23,81 @@ module.exports  = React.createClass({
         <ul>
           <li>{this.printLanguagesButtons()}</li>
           <li>
-            <input type="search" ref="search" autofocus placeholder="TV, responsive, linked data …" />
-            <button type="button">Search</button>
+            <input type="search" ref="search" onChange={this.executeSearch} autofocus placeholder="TV, responsive, linked data …" />
+            <button type="button" onClick={this.executeSearch}>Search</button>
           </li>
         </ul>
       </nav>
     );
   },
 
+  componentWillUpdate: function(nextProps, nextState){
+    if (this.state === nextState){
+      return false;
+    }
+
+    this.notifyUpdate({
+      search: nextState.search,
+      languages: nextState.languagesWhitelist
+    });
+  },
+
+  notifyUpdate: function(filters){
+    this.props.onUpdate(function(repos){
+      return repos.filter(function(repo){
+        if (filters.languages.length && filters.languages.indexOf(repo.language) === -1) {
+          return false;
+        }
+
+        if (filters.search){
+          var re = new RegExp(filters.search.split(' ').join('.+'), 'i');
+
+          if (!(repo.name + ' ' + repo.description).match(re)){
+            return false;
+          }
+        }
+
+        return true;
+      });
+    });
+  },
+
+  executeSearch: function(){
+    var node = this.refs.search.getDOMNode();
+
+    this.setState({ search: node.value.trim() });
+  },
+
+  toggleLang: function(lang){
+    var activeLangs = this.state.languagesWhitelist;
+
+    if (this.isLangActive(lang)) {
+      activeLangs = activeLangs.filter(function(l){
+        return l !== lang;
+      });
+    }
+    else {
+      activeLangs.push(lang);
+    }
+
+    this.setState({ languagesWhitelist: activeLangs });
+  },
+
+  isLangActive: function(lang){
+    return this.state.languagesWhitelist.indexOf(lang) !== -1;
+  },
+
   printLanguagesButtons: function(){
     var languages = this.getLanguagesFromList(this.props.repos);
 
     return languages.map(function(lang){
-      return (<button><Language id={lang} /></button>)
+      var classNames = cx({
+        'selected': this.isLangActive(lang)
+      });
+
+      return (<button onClick={this.toggleLang.bind(this, lang)} className={classNames}>
+        <Language id={lang} />
+      </button>)
     }, this);
   },
 
